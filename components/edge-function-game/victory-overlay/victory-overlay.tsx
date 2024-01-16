a233dc9f7d337c676ea84022a4dc5c278a8db1cf
Jason K Frank <jasonkylefrank@users.noreply.github.com>
2024-01-16 12:56:10 -0500

    Added data section to the Victory Overlay

--- components/edge-function-game/victory-overlay/victory-overlay.tsx
+++ components/edge-function-game/victory-overlay/victory-overlay.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
   motion,
   Variants,
@@ -11,6 +9,8 @@ import HeartBrokenGlyph from "components/icon-glyphs/heart_broken-glyph";
 import HeartGlyph from "components/icon-glyphs/heart-glyph";
 import { useMediaQuery } from "lib/useMediaQuery";
 import EastGlyph from "components/icon-glyphs/east-glyph";
+import { NetworkLatency, NetworkType } from "../edge-function-game";
+import DataRow from "./data-row";
 
 //#region --- Framer Motion Variants ---
 interface OverlayVariants extends Variants {
@@ -189,11 +189,57 @@ function updateVariantsForScreenSize(isSmallScreen: boolean) {
 
 export default function VictoryOverlay({
   onRestartGame,
+  networkLatencies,
 }: {
   onRestartGame: () => void;
+  networkLatencies: NetworkLatency[];
 }) {
   const animationControls = useAnimationControls();
   const isSmallScreen = useMediaQuery("(max-width: 640px)");
+
+  const firstEdgeCallLatency = networkLatencies.find(
+    (item) => item.networkType === NetworkType.VercelEdge
+  )?.latency;
+  const firstServerlessCallLatency = networkLatencies.find(
+    (item) => item.networkType === NetworkType.VercelServerlessAustralia
+  )?.latency;
+
+  const getAverageLatency = (networkType: NetworkType) => {
+    const networkTypeCount = networkLatencies.reduce(
+      (count: number, item) =>
+        item.networkType === networkType ? count + 1 : count,
+      0
+    );
+
+    const averageLatency =
+      networkLatencies.reduce(
+        (accum: number, item) =>
+          item.networkType === networkType ? accum + item.latency : accum,
+        0
+      ) / networkTypeCount;
+
+    return Math.round(averageLatency);
+  };
+
+  const getNetworkTypeLatencies = (networkType: NetworkType) =>
+    networkLatencies.reduce(
+      (accum: number[], item) =>
+        item.networkType === networkType ? [...accum, item.latency] : accum,
+      []
+    );
+
+  const getLongestLatency = (networkType: NetworkType) => {
+    const networkTypeLatencies = getNetworkTypeLatencies(networkType);
+    return networkTypeLatencies.length
+      ? Math.max(...networkTypeLatencies)
+      : undefined;
+  };
+  const getShortestLatency = (networkType: NetworkType) => {
+    const networkTypeLatencies = getNetworkTypeLatencies(networkType);
+    return networkTypeLatencies.length
+      ? Math.min(...getNetworkTypeLatencies(networkType))
+      : undefined;
+  };
 
   useEffect(() => {
     async function runAnimationStates() {
@@ -216,6 +262,8 @@ export default function VictoryOverlay({
 
   // const heartSizeClasses = "h-[80px] w-[80px] sm:h-[132px] sm:w-[132px]";
   const heartSizeClasses = "h-[72px] w-[72px] sm:h-[112px] sm:w-[112px]";
+  const statsHeaderSideBorderClassNames =
+    "w-full border-1 border-black/20 inline-block";
 
   return (
     <motion.div
@@ -225,7 +273,7 @@ export default function VictoryOverlay({
     >
       <motion.div
         variants={coloredScrimVariants}
-        className="grid h-full grid-rows-[auto_1fr_auto] place-items-center bg-[#08eeb2] opacity-0"
+        className="grid h-full grid-rows-[auto_1fr_auto] place-items-center overflow-y-scroll bg-[#08eeb2] opacity-0"
       >
         <header className="flex h-20 place-items-center">
           <motion.a
@@ -267,17 +315,51 @@ export default function VictoryOverlay({
           </motion.h3>
         </div>
 
-        <motion.div variants={otherElementsVariants}>
-          <h4 className="mt-8 text-black/60">{"Network stats for nerds"}</h4>
-          <p className="mt-4">{""}</p>
-          {[1, 2, 3, 4].map((i) => (
-            <p key={i} className="text-black/80">
-              {"nerdy stat will be here..."}
-            </p>
-          ))}
-          <p className="mb-8"></p>
+        <motion.div
+          variants={otherElementsVariants}
+          className="max-w-xl text-base"
+        >
+          <div className="mt-8 grid w-full grid-cols-[1fr_auto_1fr] place-items-center gap-6">
+            <hr className={`${statsHeaderSideBorderClassNames}`} />
+            <h4 className="text-black/50">{"Network stats for nerds"}</h4>
+            <hr className={`${statsHeaderSideBorderClassNames}`} />
+          </div>
+
+          <div className="not-prose mb-12 mt-6 grid grid-cols-[auto_auto_auto] gap-x-6 gap-y-2">
+            <span />
+            <span className="text-right">{"EDGE"}</span>
+            <span className="text-right">{"SERVERLESS"}</span>
+
+            <DataRow
+              label="First request latency"
+              edgeValue={firstEdgeCallLatency}
+              serverlessValue={firstServerlessCallLatency}
+            />
+            <DataRow
+              label="Average latency"
+              edgeValue={getAverageLatency(NetworkType.VercelEdge)}
+              serverlessValue={getAverageLatency(
+                NetworkType.VercelServerlessAustralia
+              )}
+            />
+            <DataRow
+              label="Longest"
+              edgeValue={getLongestLatency(NetworkType.VercelEdge)}
+              serverlessValue={getLongestLatency(
+                NetworkType.VercelServerlessAustralia
+              )}
+            />
+            <DataRow
+              label="Shortest"
+              edgeValue={getShortestLatency(NetworkType.VercelEdge)}
+              serverlessValue={getShortestLatency(
+                NetworkType.VercelServerlessAustralia
+              )}
+            />
+          </div>
         </motion.div>
       </motion.div>
+      <div>{"&nbsp;"}</div>
     </motion.div>
   );
 }
